ab0104fc45b33c7f131d8acd2714c0683a38c5cc
M.Bakhterev <mob@k.imm.uran.ru>
2019-03-23 01:11:03 +0500

    WIP: Разделение commands на управляемые части

---
+++ inc/gnu.mk
@@ -0,0 +1,11 @@
+tex-common := \
+	commands.tex \
+	cmd-engine.tex \
+	format-phd.tex \
+	format-a5.tex \
+	listings.tex \
+	preamble.tex \
+	scm-bigops.tex \
+	scm-indexes.tex \
+	scm-enum.tex \
+	scm-keywords.tex
