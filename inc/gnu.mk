082add21a695a95be1c21c9e5ff9625f2616ae80
mbakhterev <mob@k.imm.uran.ru>
2019-04-02 22:57:14 +0500

    WIP: Структурирование команд

--- inc/gnu.mk
+++ inc/gnu.mk
@@ -1,12 +1,15 @@
 tex-common := \
 	commands.tex \
-	cmd-engine.tex \
-	format-phd.tex \
-	format-a5.tex \
+	engine.tex \
 	listings.tex \
 	preamble.tex \
-	scm-bigops.tex \
-	scm-indexes.tex \
-	scm-enum.tex \
-	scm-keywords.tex \
-	cmd-domains.tex
+	schemes/bigops.tex \
+	schemes/indexes.tex \
+	schemes/enum.tex \
+	schemes/keywords.tex \
+	schemes/lambda.tex \
+	commands/domains.tex \
+	commands/pars.tex \
+	commands/lambda.tex \
+	formats/phd.tex \
+	formats/a5.tex 
