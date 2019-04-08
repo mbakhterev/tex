1a7f6debadc291b3e77623cdb7a25a125654a14f
mbakhterev <mob@k.imm.uran.ru>
2019-04-08 19:21:27 +0500

    WIP: Структурирование команд. Менее развесистое дерево

--- inc/gnu.mk
+++ inc/gnu.mk
@@ -5,11 +5,13 @@ tex-common := \
 	preamble.tex \
 	schemes/bigops.tex \
 	schemes/indexes.tex \
-	schemes/enum.tex \
 	schemes/keywords.tex \
-	schemes/lambda.tex \
 	commands/domains.tex \
 	commands/pars.tex \
 	commands/lambda.tex \
+	commands/enum.tex \
 	formats/phd.tex \
 	formats/a5.tex 
+
+# schemes/enum.tex \
+# schemes/lambda.tex \
